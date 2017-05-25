981c3dd687627623c110e40cfb688e9f21dddd6d
blulaser <blulaser@naver.com>
2017-05-25 17:20:35 +0900

    eee

--- app/src/main/java/com/example/administrator/myapplication/ShareActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/ShareActivity.java
@@ -1,30 +1,20 @@
 package com.example.administrator.myapplication;
 
+
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Environment;
 
-import java.io.File;
 
-/**
- * Created by Administrator on 2017-05-22.
- */
 
 public class ShareActivity {
     Intent share()
     {
-        File fileRoute = null;
-        fileRoute = Environment.getExternalStorageDirectory();
 
         Intent intent = new Intent();
-
         intent.setAction(Intent.ACTION_SEND);
 
-
-
-        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(fileRoute+"/temp/"+"1.jpg"));
-        intent.setType("image/*");
-
+        intent.setType("application/octet-stream");
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///storage/emulated/0/temp/1.ics"));
 
         Intent chooser = Intent.createChooser(intent, "공유");
 
